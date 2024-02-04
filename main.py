4a989dbf080dea358dc87d5647e74dfe3db465cd
Bancheer <andreypronko2002@gmail.com>
2024-02-04 17:29:24 +0100

    gg

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request
 import json
 from datetime import datetime
 
+
 app = Flask(__name__, static_url_path='/static')
+@app.route('/')
 def index():
     return render_template('index.html')
 
